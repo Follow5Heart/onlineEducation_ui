47bb80578ccd8dd0008589c47a999c780777f19d
ztyCode <1793959949@qq.com>
2023-02-08 22:15:58 +0800

    修改了导航栏中头像个人中心的首页路由

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -11,7 +11,7 @@
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <router-link to="/">
+          <router-link to="/home">
             <el-dropdown-item>
               首页
             </el-dropdown-item>
